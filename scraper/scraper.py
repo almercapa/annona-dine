c20858b2620464d54149a28e3623dfe7041f5289
Almer CS <almer@imac.myfiosgateway.com>
2026-05-18 19:56:17 -0400

    Created scraper for all three dining halls

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -1,19 +1,36 @@
 import requests
 import json
+import datetime
 
-response = requests.get("https://rutgers.api.nutrislice.com/menu/api/weeks/school/livingston-dining-commons/menu-type/dinner/2026/04/28/")
+def scrape_menu(hall, type):
+    now = datetime.datetime.now()
+    date = now.strftime("%Y/%m/%d")
+    link = f"https://rutgers.api.nutrislice.com/menu/api/weeks/school/{hall}/menu-type/{type}/{date}"
+    try:
+         response = requests.get(link)
+    except:
+         print("API is down")
+         return
 
-data = response.json()
+    data = response.json()
 
-meals = []
+    meals = []
 
-for item in data['days'][0]['menu_items']:
-    if not item['is_station_header'] and item['food'] is not None:
-        meal = {
-            "name": item['food']['name'],
-            "calories": item['food']['rounded_nutrition_info']['calories'],
-            "protein": item['food']['rounded_nutrition_info']['g_protein']
-        }
-        meals.append(meal)
-    with open("menu_clean.json", "w") as f:
-        json.dump(meals, f, indent=2) 
+    date = now.strftime("%Y-%m-%d")
+    for day in data['days']:
+         if day['date'] == date:
+               for item in day['menu_items']:
+                    if not item['is_station_header'] and item['food'] is not None:
+                        meal = {
+                            "name": item['food']['name'],
+                            "calories": item['food']['rounded_nutrition_info']['calories'],
+                            "protein": item['food']['rounded_nutrition_info']['g_protein']
+                        }
+                        meals.append(meal)
+    
+    if not meals:
+         print("No items available")
+         return
+    
+    with open(f"{hall}_{type}.json", "w") as f:
+            json.dump(meals, f, indent=2) 
