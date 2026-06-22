3b510dd720dedf8a0c82cfc3cb65fb6d3bf86580
Almer CS <almer@imac.myfiosgateway.com>
2026-06-22 15:18:09 -0400

    Added item names to endpoint via JOIN on items table

--- backend/main.py
+++ backend/main.py
@@ -9,7 +9,6 @@ import datetime
 
 app = FastAPI() # Creates app instance
 Base.metadata.create_all(bind=engine) # Creates all tables on startup
-
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 def get_db():
@@ -24,5 +23,5 @@ def get_menu(hall: str, food_type: str, db: Annotated[Session, Depends(get_db)])
     now = datetime.datetime.now()
     date = now.strftime("%Y-%m-%d")
     hallID = db.execute(select(DiningHall).where(DiningHall.slug == hall)).scalars().first()
-    appearance = db.execute(select(Appearance).where(Appearance.hall_id==hallID.id).where(Appearance.date == date).where(Appearance.food_type == food_type)).scalars().all()
-    return appearance
+    appearance = db.execute(select(Appearance.item_id, Appearance.calories, Appearance.fat, Appearance.carbs, Appearance.food_type, Item.name).join(Item).where(Appearance.hall_id==hallID.id).where(Appearance.date == date).where(Appearance.food_type == food_type)).all()
+    return [dict(row._mapping) for row in appearance]
