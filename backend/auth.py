9f1f16c4c5edc4f8a6a28f493c6d50a364c87ff4
Almer CS <almer@imac.myfiosgateway.com>
2026-06-30 16:40:01 -0400

    Added comments/refactored auth.py

--- backend/auth.py
+++ backend/auth.py
@@ -1,29 +1,30 @@
-import datetime
-import httpx
-from fastapi import APIRouter, Depends, HTTPException, status
-from fastapi.responses import RedirectResponse
-from fastapi.security import OAuth2PasswordBearer
-from sqlalchemy.orm import Session
-from sqlalchemy import select
-from typing import Annotated
-from jose import jwt
-import jwt as pyjwt
+import datetime # used to find the date
+import httpx # used to fetch/retrieve data from external APIs
+from fastapi import APIRouter, Depends, HTTPException, status # used to retrieve specific routes for router, use for database dependency, return errors and specific statuses
+from fastapi.responses import RedirectResponse # used to send user to different url
+from fastapi.security import OAuth2PasswordBearer # used to retrieve jwt from Authorization header on protected requests
+from sqlalchemy.orm import Session # used for type hinting
+from sqlalchemy import select # used in queries
+from typing import Annotated # using as a parameter for db
+from jose import jwt # used to decode third party tokens
+import jwt as pyjwt # used to encode/decode self created tokens
+# importing objects, functions, and variables from other files
 from models import User
 from database import get_db
 from config import CLIENT_ID, CLIENT_SECRET, SECRET_KEY, REDIRECT_URI, ALGORITHM, JWT_ALGORITHM
 
-router = APIRouter()
+router = APIRouter() # initializes router
 
 @router.get("/auth/google")
 def get_auth():
-    return RedirectResponse(url=f"https://accounts.google.com/o/oauth2/auth?scope=email%20profile&response_type=code&client_id={CLIENT_ID}&redirect_uri={REDIRECT_URI}")
+    return RedirectResponse(url=f"https://accounts.google.com/o/oauth2/auth?scope=email%20profile&response_type=code&client_id={CLIENT_ID}&redirect_uri={REDIRECT_URI}") #redirects user to google auth page
 
 @router.get("/auth/google/callback")
-def get_callback(code:str,db: Annotated[Session, Depends(get_db)]):
-    res = httpx.post(url="https://oauth2.googleapis.com/token", data={"client_id":CLIENT_ID, "client_secret":CLIENT_SECRET,"code":code,"redirect_uri":REDIRECT_URI,"grant_type":"authorization_code"})
+def get_callback(code:str, db: Annotated[Session, Depends(get_db)]):
+    res = httpx.post(url="https://oauth2.googleapis.com/token", data={"client_id":CLIENT_ID, "client_secret":CLIENT_SECRET,"code":code,"redirect_uri":REDIRECT_URI,"grant_type":"authorization_code"}) # Sends user data to token url
     token_data = res.json()
-    id = token_data["id_token"]
-    user_info=jwt.decode(id,"",[ALGORITHM],options={"verify_signature": False, "verify_at_hash": False},audience=CLIENT_ID)
+    id_token = token_data["id_token"]
+    user_info=jwt.decode(id_token,"",[ALGORITHM],options={"verify_signature": False, "verify_at_hash": False},audience=CLIENT_ID)
     if user_info["hd"] != "scarletmail.rutgers.edu":
         raise HTTPException(
             status_code=status.HTTP_403_FORBIDDEN, 
@@ -43,28 +44,27 @@ def get_callback(code:str,db: Annotated[Session, Depends(get_db)]):
     payload = {
         "sub": user_info["email"],
         "exp": datetime.datetime.now(datetime.timezone.utc) + datetime.timedelta(days=30)
-    }
-    encoded = pyjwt.encode(payload, SECRET_KEY, algorithm=JWT_ALGORITHM)
+    } # JWT payload containing user email and expiration date
+    encoded = pyjwt.encode(payload, SECRET_KEY, algorithm=JWT_ALGORITHM) # Encodes JWT
 
     return encoded
-
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/google/callback")
+ 
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/google/callback") # Retrieves JWT from Auth header
 def get_current_user(db: Annotated[Session, Depends(get_db)], token: str = Depends(oauth2_scheme)):
-    try:
-            current_user=pyjwt.decode(token,SECRET_KEY,[JWT_ALGORITHM])
-            current_sub = current_user["sub"]
-            query = (
-                select(User)
-                .where(User.email==current_user["sub"])
+    try: # only runs if the token is valid
+        current_user=pyjwt.decode(token,SECRET_KEY,[JWT_ALGORITHM]) # decodes JWT to find user data
+        query = (
+            select(User)
+            .where(User.email==current_user["sub"])
+        )
+        sub_check = db.execute(query).scalars().first() # queries user email
+        if sub_check:
+            return sub_check
+        else: # returns invalid if email not in database
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED, 
+                detail="Not a current user"
             )
-            sub_check = db.execute(query).scalars().first()
-            if sub_check:
-                return sub_check
-            else:
-                raise HTTPException(
-                    status_code=status.HTTP_401_UNAUTHORIZED, 
-                    detail="Not a current user"
-                )
     except pyjwt.InvalidTokenError:
          raise HTTPException(
                     status_code=status.HTTP_401_UNAUTHORIZED, 
