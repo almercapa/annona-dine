05c6d08cb3fd8fa16061adfdf6a76a9d3a69add8
Almer CS <almer@imac.myfiosgateway.com>
2026-06-30 17:02:40 -0400

    Added comments/refactored database.py and config.py

--- backend/config.py
+++ backend/config.py
@@ -1,10 +1,10 @@
-from dotenv import load_dotenv
-import os
+from dotenv import load_dotenv # used to load env file
+import os # used to interact with operating system, specifically env file
 
-load_dotenv()
-CLIENT_ID=os.getenv("GOOGLE_CLIENT_ID")
-CLIENT_SECRET=os.getenv("GOOGLE_CLIENT_SECRET")
-SECRET_KEY=os.getenv("SECRET_KEY")
-REDIRECT_URI="http://localhost:8000/auth/google/callback"
+load_dotenv() # loads env file
+CLIENT_ID=os.getenv("GOOGLE_CLIENT_ID") # defines name of the app to Google
+CLIENT_SECRET=os.getenv("GOOGLE_CLIENT_SECRET") # app password for verification to Google
+SECRET_KEY=os.getenv("SECRET_KEY") # for signing Annona's JWTs
+REDIRECT_URI="http://localhost:8000/auth/google/callback" # for site redirecting
 ALGORITHM = "RS256"  # for decoding Google's token
 JWT_ALGORITHM = "HS256"  # for encoding Annona's token
--- backend/database.py
+++ backend/database.py
@@ -1,11 +1,11 @@
-import os
-from dotenv import load_dotenv
-from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base, sessionmaker
+import os # used to read variables/files
+from dotenv import load_dotenv # used to load env files
+from sqlalchemy import create_engine # used to create engine
+from sqlalchemy.orm import declarative_base, sessionmaker # used to create blueprint and database templates
 
 load_dotenv() # Pulls all global environment data from env file
 database_url = os.getenv("DATABASE_URL") # Attaches database url from env file to a variable
-engine = create_engine(database_url, pool_pre_ping=True) # Creates connection to Neon using database url
+engine = create_engine(database_url, pool_pre_ping=True) # Creates connection to Neon using database url, use for database interactions, pool_pre_ping tests connection before using it
 Base = declarative_base() # Blueprint for all SQLAlchemy models
 
 try: # Testing connection
@@ -14,12 +14,12 @@ try: # Testing connection
 except Exception as e:
     print(f"Connection failed: {e}")
 
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine) # Creates a template for database sessions
 
-def get_db():
+def get_db(): # Opens session for route
     db = SessionLocal()
     try:
-        yield db
-    finally:
-        db.close()
+        yield db # Keeps database open while route is using it
+    finally: 
+        db.close() # Closes once route is completed
 
